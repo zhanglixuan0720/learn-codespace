4b4198d8e1c9464a0d65702fc2dde0a03fb5f2f7
Lixuan Zhang <44504765+zhanglixuan0720@users.noreply.github.com>
2025-05-01 14:20:03 +0000

    modify add

---
+++ src/Components/Education.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Education = () => {
+    return(
+        <section className="light" id="education">
+            <h2>Education</h2>
+            <ul>
+                <li>
+                    <h3>PhD in Computer Science</h3>
+                    <p>University of Example - 2020 to 2025</p>
+                </li>
+                <li>
+                    <h3>Bachelor's in Software Engineering</h3>
+                    <p>Example Institute of Technology - 2016 to 2020</p>
+                </li>
+            </ul>
+        </section>
+    );
+}
+
+export default Education;
